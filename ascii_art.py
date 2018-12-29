12f8c47f5de44e6f15f9a59edb31ec4913506431
mrychlicka <rychlickam@gmail.com>
2018-12-29 18:41:17 +0100

    Added ASCII art

---
+++ ascii_art.py
@@ -0,0 +1,53 @@
+class CoffeeDrinksAscii:
+
+    def print_coffee_beans(self):
+        print "        .-."
+        print "  ..==./   \\"
+        print " /     \    |"
+        print " \  |  /   / ---."
+        print "  `'==''---;     \\"
+        print "           \     /"
+        print "            '--'`"
+
+    def print_water(self):
+        print "    .----."
+        print "    |~~~~|"
+        print "    |    |"
+        print "    |____|"
+
+    def print_milk(self):
+        print "      _____"
+        print "     j_____j"
+        print "    /_____/_\\"
+        print "    |_(~)_| |"
+        print "    | )\"( | |"
+        print "    |(@_@)| |"
+        print "    |_____|,'"
+
+    def print_trashcan(self):
+        print "    ==^=="
+        print "    |[[[|"
+        print "    |[[[|"
+        print "    '---'"
+
+    def print_espresso(self):
+        print "     ( (     "
+        print "      ) )    "
+        print "    (----)-) "
+        print "     \__/-'  "
+        print "    `----'   "
+
+    def print_capuccino(self):
+        print "      ( (      "
+        print "       ) )     "
+        print "    (^v^v^v)-) "
+        print "     \____/-'  "
+        print "    `------'   "
+
+    def print_latte_macchiato(self):
+        print "     ( ("
+        print "     _)_)"
+        print "    |====|-+"
+        print "    |\/\/| |"
+        print "    |====|-+"
+        print "    \____/"
